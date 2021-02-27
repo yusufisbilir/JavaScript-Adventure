e1d0250bfaaa4cb39d4e97bebcdf0d7a63e51573
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-02-27 18:30:38 +0300

    fix

--- src/10-arrayFunctions.js
+++ src/10-arrayFunctions.js
@@ -12,16 +12,16 @@ abc.shift(); // first element deleted
 
 abc.unshift("new element"); // element added to the top
 
-delete abc[2];
+delete abc[2];  // 2. index deleted
 
 abc.splice(4, 0, "1", "2", "3"); // start from 4, delete 0 element , add "1","2","3"
-
 console.log(abc);
+
 
 let odd = [1, 3, 5];
 let even = [2, 4, 6];
 
-let combine = odd.concat(even);
+let combine = odd.concat(even); //combining
 combine = combine.sort();
 console.log(combine);
 
