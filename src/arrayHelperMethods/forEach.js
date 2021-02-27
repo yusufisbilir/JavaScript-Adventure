81c57816e1cf6b03038e7530981c7253f676d480
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-02-28 00:44:40 +0300

    fix

---
+++ src/arrayHelperMethods/forEach.js
@@ -0,0 +1,19 @@
+// forEach
+let numbers = [1,2,4,5];
+
+numbers.forEach((item, index)=>{
+    console.log(item, index);
+});
+
+
+//mine forEach function
+logArr(numbers, function(item,index){
+    console.log(item,index);
+});
+
+function logArr(array, callback){
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i], i)
+        
+    };
+};
