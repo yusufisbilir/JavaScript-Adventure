f5061340511b30990a9107c760f7f585de0e1075
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-02 15:09:33 +0300

    file added

---
+++ src/dom/domRotation.js
@@ -0,0 +1,17 @@
+let val;
+const list1 = document.querySelector('ul.list');
+console.log(list1);
+
+val = list1.childNodes[1].nodeType;
+// 1 element
+// 3 text
+// 8 comment
+
+val = list1.firstElementChild;
+
+
+// parent element
+val = list1.parentElement;
+
+
+console.log(val);
